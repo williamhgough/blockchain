6cef2dedb1ce4ccbe7baee5c293fe049c2642141
William Gough <williamhenrygough@gmail.com>
2017-08-30 22:03:16 +0100

    Start boltDB integration & Cobra

--- block.go
+++ block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -10,6 +13,30 @@ type Block struct {
 	PrevBlockHash []byte
 	Hash          []byte
 	Nonce         int
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"os"
+
+	"github.com/williamhgough/blockchain/cmd"
 )
 
 func main() {
-	bc := NewBlockchain()
-
-	bc.AddBlock("Send 1 BTC to Will")
-	bc.AddBlock("Send 2 BTC to Will")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-
-		pow := NewProofOfWork(block)
-		fmt.Printf("Valid: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
